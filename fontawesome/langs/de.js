ea327748e33485488033778e06fc3bab3b5310c9
Cliff Parnitzky <github@cliff-parnitzky.de>
2015-05-06 12:49:43 +0200

    Adding german translation

---
+++ fontawesome/langs/de.js
@@ -0,0 +1,17 @@
+tinymce.addI18n('de', {
+    'Icons': 'Font-Awesome Icons',
+    'Web Application': 'Web-Anwendung',
+    'File Type': 'Dateitypen',
+    'Spinner': 'Drehend',
+    'Form Control': 'Formularfelder',
+    'Currency': 'W&auml;hrungen',
+    'Text Editor': 'Texteditor',
+    'Directional': 'Richtung',
+    'Video Player': 'Videoplayer',
+    'Brand': 'Marken',
+    'Medical': 'Medizinisch',
+    'Transportation': 'Transport',
+    'Gender': 'Geschlecht',
+    'Payment': 'Bezahlung',
+    'Chart': 'Diagramm'
+});
