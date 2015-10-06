74e0146b66a2f69e8e3c8edee0f996cf0bc2bde0
bbfriend <info@u-tec.org>
2015-10-07 00:49:44 +0900

    Create ja.js

---
+++ fontawesome/langs/ja.js
@@ -0,0 +1,18 @@
+tinymce.addI18n('ja', {
+    'Icons': 'Icon',
+    'Web Application': '一般',
+    'File Type': 'ファイルタイプ',
+    'Spinner': 'Spinner', /** Don't Change! Not rotate v1.6.0 **/
+    'Form Control': 'Formコントロール',
+    'Currency': '通貨',
+    'Text Editor': 'エディタ',
+    'Directional': '矢印・方向',
+    'Video Player': 'プレイヤー',
+    'Brand': 'ブランド',
+    'Medical': '医療',
+    'Transportation': '交通',
+    'Gender': '性',
+    'Payment': '支払い',
+    'Chart': 'チャート',
+    'Hand': '手'
+});
